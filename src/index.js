f7822b6e0f350235960e52de19879eed30fe8588
Liz P <parisielizabeth@gmail.com>
2021-01-26 04:07:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  (<ClickityClick />),
   document.getElementById('root')
 );
